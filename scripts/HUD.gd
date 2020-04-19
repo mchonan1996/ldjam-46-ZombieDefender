50cf881ba8beb6c9f8a777d46135f4c0440348b0
Michael Honan <mc.honan1996@gmail.com>
2020-04-19 19:09:23 +1000

    Improvements

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,7 +1,12 @@
 extends CanvasLayer
 class_name HUD
 
+#onready var pistol_ammo_label = $HBoxContainer/PistolAmmo/PistolAmmoLabel
+onready var shotgun_ammo_label = $HBoxContainer/ShotgunAmmo/ShotgunAmmoLabel
+onready var rocket_ammo_label = $HBoxContainer/RocketAmmo/RocketAmmoLabel
+
 func _ready() -> void:
+	#base_ammo_text = $HBoxContainer/Ammo/AmmoLabel.text
 	pass
 
 
@@ -9,5 +14,6 @@ func update_money(money: int) -> void:
 	$HBoxContainer/Money/MoneyLabel.text = "Money: $%d" % money
 
 
-func update_ammo(ammo: int) -> void:
-	$HBoxContainer/Ammo/AmmoLabel.text = "Ammo: %d" % ammo
+func update_ammo(shotgun_ammo: int, rocket_ammo: int) -> void:
+	#shotgun_ammo_label.text = 
+	pass
